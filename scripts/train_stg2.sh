7c80f7ad7f50cb2ed817470bc9c15d9d37a157b5
Thomas Benzshawel <benzshawelt@msoe.edu>
2024-02-16 12:27:46 -0600

    fixed adam error

--- scripts/train_stg2.sh
+++ scripts/train_stg2.sh
@@ -36,7 +36,7 @@
 
 # Continue training
 python3 train_stg2.py --model ORIG_STG2 --experiment sgd_trueWD_restart_cont1 \
-	--loadPath ORIG_STG1_sgd_trueWD_restart \
+	--loadPath ORIG_STG1_adam_trueWD_restart \
 	--chunkSize 100 --batchSize 32 --saveEpoch 10 \
 	--optim sgd --trueWD 1e-4 --lr 1e-1 \
 	--lrSched restart --T_0 5 --T_mult 2 --lrBase 5e-3 \
