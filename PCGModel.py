1f5f097b562a7cb60f6748c1d9f2304cc40001a0
Thomas Benzshawel <benzshawelt@msoe.edu>
2024-02-20 12:54:20 -0600

    maybe this fixes things?

--- PCGModel.py
+++ PCGModel.py
@@ -99,7 +99,7 @@ class Decoder(nn.Module):
         x = self.deconv2(F.interpolate(x, scale_factor=2))
         x = self.deconv3(F.interpolate(x, scale_factor=2))
         x = self.deconv4(F.interpolate(x, scale_factor=2))
-        x = self.deconv5(F.interpolate(x, scale_factor=2))
+        # x = self.deconv5(F.interpolate(x, scale_factor=2))
         x = self.pixel_conv(x) + self.pixel_bias.to(x.device)
         XYZ, maskLogit = torch.split(
             x, [self.outViewN * 3, self.outViewN], dim=1)
@@ -123,7 +123,7 @@ class Structure_Generator(nn.Module):
     def forward(self, x):
         latent = self.encoder(x)
         XYZ, maskLogit = self.decoder(latent)
-        
+
         return XYZ, maskLogit
 
 
