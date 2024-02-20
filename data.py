ffd0408b0fc0c1a591fb3b02188425e9f877f41b
Thomas Benzshawel <benzshawelt@msoe.edu>
2024-02-20 09:48:50 -0600

    testing

--- data.py
+++ data.py
@@ -132,8 +132,7 @@ class PointCloud2dDataset(Dataset):
         depthGT = torch.from_numpy(depthGT).permute((0,3,1,2))
         maskGT = torch.from_numpy(maskGT).permute((0,3,1,2))
 
-        groundTruth = [batch_n["image_in"][modelIdx, angle] for angle in eval_angleIdx]
-        print(f"groundTruth: {groundTruth.shape}")
+        groundTruth = batch_n["image_in"][modelIdx, eval_angleIdx]
 
         return {
             "inputImage": images,
