7d182b68a66609fbfb19a802557d8011f06b3aba
Thomas Benzshawel <benzshawelt@msoe.edu>
2024-02-23 14:49:37 -0600

    Finalized add changes

--- trainer.py
+++ trainer.py
@@ -224,7 +224,8 @@ class TrainerStage1:
                 if self.cfg.trueWD is not None:
                     for group in optimizer.param_groups:
                         for param in group['params']:
-                            param.data = param.data.add(param.data, -self.cfg.trueWD * group['lr'])
+                            # param.data = param.data.add(param.data, -self.cfg.trueWD * group['lr'])
+                            param.data = torch.add(input=param.data, other=param.data, alpha=-self.cfg.trueWD * group['lr'])
                 optimizer.step()
 
             losses.append(loss.item())
@@ -314,7 +315,8 @@ class TrainerStage2:
                 if self.cfg.trueWD is not None:
                     for group in optimizer.param_groups:
                         for param in group['params']:
-                            param.data = param.data.add(param.data, -self.cfg.trueWD * group['lr'])
+                            # param.data = param.data.add(param.data, -self.cfg.trueWD * group['lr'])
+                            param.data = torch.add(input=param.data, other=param.data, alpha=-self.cfg.trueWD * group['lr'])
                 optimizer.step()
 
             if self.on_after_batch is not None:
@@ -444,8 +446,9 @@ class TrainerStage2:
                 if self.cfg.trueWD is not None:
                     for group in optimizer.param_groups:
                         for param in group['params']:
-                            param.data = param.data.add(param.data,
-                                -self.cfg.trueWD * group['lr'])
+                            # param.data = param.data.add(param.data,
+                            #     -self.cfg.trueWD * group['lr'])
+                            param.data = torch.add(input=param.data, other=param.data, alpha=-self.cfg.trueWD * group['lr'])
                 optimizer.step()
 
             losses.append(loss.item())
