30eb43968a75f3ae1a379ccf542bbbb032d41c0b
Thomas Benzshawel <benzshawelt@msoe.edu>
2024-02-27 11:11:32 -0600

    Evaluating correct models

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -1,5 +1,5 @@
 python3 evaluate.py --model ORIG_STG2 --experiment adam_trueWD_cyclical \
-	--loadPath ORIG_STG2_adam_trueWD_cyclical \
+	--loadPath ORIG_STG2_adam_trueWD\
 	--chunkSize 32 --batchSize 32 \
 	--gpu 1
 
