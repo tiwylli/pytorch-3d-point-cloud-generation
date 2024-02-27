7c25cbbdc90b0e9ea9c2e20ad00e86090056ab80
Thomas Benzshawel <benzshawelt@msoe.edu>
2024-02-27 11:08:34 -0600

    Added Gpus to eval

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -1,7 +1,7 @@
 python3 evaluate.py --model ORIG_STG2 --experiment adam_trueWD_cyclical \
 	--loadPath ORIG_STG2_adam_trueWD_cyclical \
 	--chunkSize 32 --batchSize 32 \
-	--gpu 0
+	--gpu 1
 
 # python3 evaluate.py --model ORIG_STG2 --experiment sgd_trueWD_restart_cont1 \
 # 	--loadPath ORIG_STG2_sgd_trueWD_restart_cont1 \
--- scripts/evaluate_dist.sh
+++ scripts/evaluate_dist.sh
@@ -1,6 +1,7 @@
 python3 evaluate_dist.py --model ORIG_STG2 --experiment adam_trueWD_cyclical \
 	--loadPath ORIG_STG2_adam_trueWD_cyclical \
-	--chunkSize 32 --batchSize 32 
+	--chunkSize 32 --batchSize 32 \
+	--gpu 1
 
 # python evaluate_dist.py --model ORIG_STG2 --experiment sgd_trueWD_restart_cont1 \
 # 	--loadPath ORIG_STG2_sgd_trueWD_restart_cont1 \
